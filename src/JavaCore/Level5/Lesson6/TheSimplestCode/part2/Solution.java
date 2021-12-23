589ab22df43f99f4792e54722da64dafdeba642a
Marek <marek.oleksik@gmail.com>
2021-12-23 19:35:20 +0100

    added lesson6 solutions

---
+++ src/JavaCore/Level5/Lesson6/TheSimplestCode/part2/Solution.java
@@ -0,0 +1,36 @@
+package JavaCore.Level5.Lesson6.TheSimplestCode.part2;
+
+/*
+The simplest code: part 2
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        SiamCat coco = new SiamCat("Coco");
+        NakedCat nakedCoco = coco.shave();
+    }
+
+    public static class NakedCat {
+    }
+
+    public static class NormalCat extends NakedCat {
+
+        public NormalCat() {
+        }
+
+        public NormalCat(String name) {
+            System.out.println("My name is " + name);
+        }
+
+        public NakedCat shave() {
+            return this;
+        }
+    }
+
+    public static class SiamCat extends NormalCat {
+        public SiamCat(String name) {
+            super(name);
+        }
+    }
+}
---
+++ src/JavaCore/Level5/Lesson6/TheSimplestCode/part3/Solution.java
@@ -0,0 +1,21 @@
+package JavaCore.Level5.Lesson6.TheSimplestCode.part3;
+
+/*
+The simplest code: part 3
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public interface Runnable {
+
+    }
+
+    public class Machine implements Runnable {
+    }
+
+    public class Car extends Machine {
+    }
+}
