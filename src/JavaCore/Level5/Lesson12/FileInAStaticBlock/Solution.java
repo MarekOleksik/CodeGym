e7b992c7f5ecb137fd49ed07c98bc90cf22d5b42
Marek <marek.oleksik@gmail.com>
2021-12-29 21:11:33 +0100

    added lesson12 next part solutions

---
+++ src/JavaCore/Level5/Lesson12/FileInAStaticBlock/Solution.java
@@ -0,0 +1,34 @@
+package JavaCore.Level5.Lesson12.FileInAStaticBlock;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+File in a static block
+
+*/
+
+public class Solution {
+    public static List<String> lines = new ArrayList<>();
+
+    static {
+        try {
+            BufferedReader input = new BufferedReader(new FileReader(Statics.FILE_NAME));
+            String line = "";
+            while ((line = input.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lines);
+    }
+}
